ac6c54b81b6238b5f42b0a93b264743da06f937b
Julien Kloers <julien.kloers@gmail.com>
2026-05-04 14:11:30 -0400

    cleaning utils

--- src/jax_backend/utils/utils.py
+++ src/jax_backend/utils/utils.py
@@ -1,5 +1,3 @@
-import pickle
-from pathlib import Path
 from typing import Any, Dict, NamedTuple, Optional, Union
 
 import jax.numpy as jnp
@@ -110,42 +108,7 @@ class ControlParams(NamedTuple):
     constraint_params: Dict = dict()
 
 
-def save_data(path_or_filename: Union[str, Path], data: object):
-    """Saves data to a file via `pickle`.
 
-    Args:
-        path_or_filename (Union[str, Path]): Path or filename of the outputfile e.g. output.dat.
-        data (object): Object to be saved.
-    """
-
-    path = Path(path_or_filename)
-    # Make sure parents directories exist
-    path.parent.mkdir(parents=True, exist_ok=True)
-
-    with open(path, "wb") as file:
-        pickle.dump(data, file)
-        print("Data saved at " + str(path))
-
-
-def load_data(path_or_filename: Union[str, Path]):
-    """Loads data object via `pickle`.
-
-    Args:
-        path_or_filename (Union[str, Path]): Path or filename of the data file e.g. output.dat.
-
-    Returns:
-        object: The data object.
-    """
-
-    with open(path_or_filename, "rb") as file:
-        data = pickle.load(file)
-
-        if isinstance(data, SolutionData):
-            # Cast arrays to jax arrays
-            class_type = type(data)
-            return class_type(*(jnp.array(attr) if isinstance(attr, np.ndarray) else attr for attr in data))
-
-        return data
 
 
 
